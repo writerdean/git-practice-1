419e322f4a498997b5e3c1e3b7070c50e27cb038
Tracy Dean Blackadder <tracey.blackadder@realestate.com.au>
2019-09-21 13:48:02 +1000

    wip

--- main.js
+++ main.js
@@ -4,5 +4,15 @@ function add(num1, num2) {
 console.log(add(35, 42))
 
 function multiply(num1, num2) {
-  return num1 * num2
+  return num1 * num2;
 }
+
+multiply(2, 3)
+
+function combine(fn1, fn2) {
+  return fn1 / fn2
+}
+
+// console.log(add(35, 42), multiply(2, 3))
+
+console.log(combine(add(35, 42), multiply(2, 3)))
