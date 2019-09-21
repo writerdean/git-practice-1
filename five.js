99dba428658ec2a26d0a681fcadfc55e413b2708
Tracy Dean Blackadder <tracey.blackadder@realestate.com.au>
2019-09-21 14:36:11 +1000

    five commited on master

--- five.js
+++ five.js
@@ -1,3 +1,7 @@
 function updateAgain() {
   return 'This was created in writerdean, after third-branch was commited but not pushed, and after fourth (created on thermo)was committed and pushed to thermo/master, and no pull rebase was done.'
 }
+
+function seven() {
+  return 'This was added to file five'
+}
